913fe549c9d55e2e8508b1037809a0bd6401d88b
Cojocareanu Cosmin-Florentin <corvus788@gmail.com>
2024-04-15 13:27:29 +0300

    firstCommit

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "/Users/cosmin/Desktop/mendix/themesource/administration/native/main";
+import * as style1 from "/Users/cosmin/Desktop/mendix/themesource/communitycommons/native/main";
+import * as style2 from "/Users/cosmin/Desktop/mendix/themesource/feedbackmodule/native/main";
+import * as style3 from "/Users/cosmin/Desktop/mendix/themesource/nanoflowcommons/native/main";
+import * as style4 from "/Users/cosmin/Desktop/mendix/themesource/atlas_core/native/main";
+import * as style5 from "/Users/cosmin/Desktop/mendix/themesource/mainmodule/native/main";
+import * as style6 from "/Users/cosmin/Desktop/mendix/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
